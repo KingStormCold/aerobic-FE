407f0b94c15a46e1d33b6fddb2aa9dce3dcef43c
dien_hv <vinhdien1234567@gmal.com>
2022-09-29 23:22:31 +0700

    done task 5 create page hotline

---
+++ src/main/webapp/app/modules/client/hotline/hotline.tsx
@@ -0,0 +1,15 @@
+import './hotline.scss';
+import React from 'react';
+
+export const HotLine = () => {
+  return (
+    <div className="tradeIn">
+      <h3>LIÊN HỆ</h3>
+      <p>Hotline đóng góp ý kiến : 0938792282</p>
+      <p>Tư vấn trả góp : 0566564570 (Tường), 0763556777 (Thịnh)</p>
+      <p>Nhân viên kỹ thuật : 0565812345</p>
+    </div>
+  );
+};
+
+export default HotLine;
